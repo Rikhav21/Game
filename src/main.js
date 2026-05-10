0dff3e724a6f2d845e4ae319e169ce50d6b1594f
Rikhav21 <167136725+Rikhav21@users.noreply.github.com>
2026-05-09 20:39:02 -0400

    deleted some extra files

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import './style.css'
+import Phaser from 'phaser'
+const config = {
+  type:Phaser.WEBGL,
+  width:500,
+  height:500,
+  canvas: document.getElementById('gameCanvas')
+}
+
+new Phaser.Game(config)
